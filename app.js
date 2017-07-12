e7e6ff1dbc7af0994e88360bcfc733dbaf561c7a
windspinks <windspinks@gmail.com>
2017-07-12 13:18:01 -0400

    scaffolding

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mustacheExpress = require("mustache-express");
 const session = require("express-session");
 const path = require("path");
 const router = require("./routes/router.js");
 
 //Express App Initialization
 const app = express();
+//Public Directory Setup
+app.use("/public", express.static(path.join(__dirname, "public")));
+//Mustache View Engine
+app.engine("mustache", mustacheExpress());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "mustache");
+app.set("layout", "layout");
 //Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(expressValidator());
 //Express Session Initialization
 app.use(session({
-  secret: "141252356 3w7s$^#%&^",
+  secret: "1412(>@52356 3w7s$^#%&^",
   resave: false,
   saveUninitialized: false
 }));
---
+++ tests/test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const assert = require('assert');
+const app = require('../app');
+const models = require('../models');
+
+describe ("User Experience", () => {
+  it("Users should be able to register", (done) => {
+    //tests here
+  });
+  it("Users should be able to login", (done) => {
+    //tests here
+  });
+  it("Users should be able to logout", (done) => {
+    //tests here
+  });
+});
